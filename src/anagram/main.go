3b7c415d0b6ae02ebe4e106c9378cd74c60eb6a6
Abradat <shayankhorsandi1376@gmail.com>
2018-03-03 19:40:07 +0330

    final version, menu added, persian input support added

--- src/anagram/main.go
+++ src/anagram/main.go
@@ -1,12 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"bufio"
+	"os"
+
+)
 
 func main() {
 
 	var dictionary = make(map[string][]string)
 
 	dictionary = generateAnagrams()
+	reader := bufio.NewReader(os.Stdin)
 
-	fmt.Print(dictionary[SortString("قلابت")])
+	for {
+		fmt.Printf("You can Do 2 things\n\n1)Anagram\n2)EXIT\n\nEnter your Decision: ")
+		text, _ := reader.ReadString('\n')
+		if(text[:len(text) - 1] == "1") {
+			fmt.Print("Enter The word for anagram: ")
+			text, _ := reader.ReadString('\n')
+			results := dictionary[SortString(text[:len(text) - 1])]
+			if(len(results) == 0) {
+				fmt.Printf("The is NO Result !!!\n")
+
+			} else {
+				fmt.Printf("The results are these : \n")
+				for cnt, result := range results {
+					fmt.Printf("%d) %s\n", cnt + 1, result)
+				}
+				fmt.Printf("\n***********\n\n")
+			}
+		} else {
+			fmt.Printf("BYE BYE :)\n")
+			break
+		}
+	}
+
+
 }
